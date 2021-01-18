702296925a6e5ee7d18db086b39b66e0dd4e806a
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-01-18 22:59:10 +0600

    trying to program

---
+++ script.js
@@ -0,0 +1,11 @@
+let addOne;
+addOne = document.getElementById('addition')
+
+let addTwo;
+addTwo = document.getElementById('additionTwo')
+
+function equallyone(){
+    let pOne;
+    pOne = document.getElementById('equallyone')
+    pOne.innerHTML = "addOne + addTwo"
+}
